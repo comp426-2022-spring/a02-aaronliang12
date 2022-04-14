93b132f69cf7a21afac2a658baa38212e6040fce
Aaron Liang <69773000+aaronliang12@users.noreply.github.com>
2022-04-13 22:34:38 -0400

    ASD

--- flips.js
+++ flips.js
@@ -2,9 +2,17 @@ import defaultExport from "./modules/coin.mjs";
 import minimist from "minimist"
 const argv = minimist(process.argv.slice(2))
 
-var numFlips = argv["number"]  || 1
+var numFlips = argv["number"]
+if(numFlips != null){
+    var arrFlips = defaultExport.coinFlips(numFlips)
+    var result = defaultExport.countFlips(arrFlips)
+    console.log(arrFlips)
+    console.log(result)
+}
+else{
+    var arrFlips = defaultExport.coinFlips(1)
+    var result = defaultExport.countFlips(arrFlips)
+    console.log(arrFlips)
+    console.log(result)
+}
 
-var arrFlips = defaultExport.coinFlips(numFlips)
-var result = defaultExport.countFlips(arrFlips)
-console.log(arrFlips)
-console.log(result)
--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ const argv = minimist(process.argv.slice(2))
 
 var call = argv["call"]
 
-if(call == null){
+if(call == null || (call != "heads" && call != "tails")){
     console.error("Error: no input");
     console.error("Usage: node guess-flip --call=[heads|tails]");
 }
